0f628fec1de2b95c28c19c349548224b578cc9b0
KARLA JIMENEZ <karla.jimenez@cetysuniversidad.edu.mx>
2025-02-27 16:51:28 -0800

    added seccion amarilla

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -97,9 +97,7 @@ export default function TabTwoScreen() {
 
 const styles = StyleSheet.create({
   headerImage: {
-    color: '#808080',
-    bottom: -90,
-    left: -35,
+    color: '#FFFFFF',
     position: 'absolute',
   },
   titleContainer: {
